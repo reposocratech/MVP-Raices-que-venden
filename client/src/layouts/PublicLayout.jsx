9ba4e0aad3a46a7803e36770c66b7f63f67045ff
Marina Catalán <marina.catalan.esteve@gmail.com>
2025-10-01 19:36:02 +0200

    cambios

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavbarApp } from '../components/NavbarApp/NavbarPublic/NavbarPublic'
+import FooterApp from '../components/FooterApp/FooterApp'
 
 export const PublicLayout = () => {
   return (
     <>
     <NavbarApp/>
+    <FooterApp/>
     </>
   )
 }
