f01831a638041a809604c79ddf0d146da2d1be6b
Marina Catalán <marina.catalan.esteve@gmail.com>
2025-10-02 13:32:47 +0200

    home

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { NavbarApp } from '../components/NavbarApp/NavbarPublic/NavbarPublic'
 import FooterApp from '../components/FooterApp/FooterApp'
+import { Outlet } from 'react-router-dom'
 
 export const PublicLayout = () => {
   return (
     <>
     <NavbarApp/>
+    <Outlet/>
     <FooterApp/>
     </>
   )
--- client/src/pages/PublicPages/Home/Home.jsx
+++ client/src/pages/PublicPages/Home/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './home.css'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    
+      <section className='home'>
+        <div className='home-container'>
+          <div className='row justify-content-center align-items-center'>
+            <div className='col-md-6'>
+              <h1 className='h1-ppal'>Raíces que venden</h1>
+              <h4>Copywriting rural con alma · By Almuyalma</h4>
+              <p>Convierto las cosas en emociones, convierto cada 
+              detalle en una historia emocional. La vida rural es lo 
+              más valioso que tenemos, en ella existe la raíz de nuestra 
+              esencia y cultura. Por ello quiero hacer crecer cualquier rincón rural en valor. </p>
+            <a href="#vermas" className='btn btn-size btn-outline-dark'>Saber más</a>
+            </div>
+          </div>
+          <div className='col-md-6'>
+            <img 
+            src="/images/home.png"
+            alt="imagen camino almendros en flor"
+            className='img-fluid img-rounded-left'
+
+            />
+
+          </div>
+
+
+        </div>
+      </section>
+    
   )
 }
 
