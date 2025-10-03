903013ac9f4184467faf61da94c21051c2a7934d
Cristina <cristina31101995@gmail.com>
2025-10-03 23:36:44 +0200

    login

--- client/src/pages/PublicPages/Auth/Login/Login.jsx
+++ client/src/pages/PublicPages/Auth/Login/Login.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Boton } from '../../../../components/Boton/Boton';
+import './login.css'
+import {Link, useNavigate} from 'react-router-dom'
+import { AuthContext } from '../../../../context/AuthContextProvider';
+import { fetchData } from '../../../../helpers/axiosHelper';
+
+
+const initialValue = {
+  email:"",
+  password: ""
+}
 
 const Login = () => {
+
+  const { setUser, setToken, setTexts } = useContext(AuthContext)
+
+  const [ login, setLogin ] = useState(initialValue);
+  const [showPass, setShowPass] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setLogin({...login, [name]:value})
+  }
+
+  const onSubmit = async(e) => {
+      e.preventDefault();
+      try {
+
+        const res = await fetchData('/user/login', 'POST', login);
+        const token = res.data.token;
+        localStorage.setItem("token", token); //guardar token
+
+        //petición al back para traer datos
+        const resUser = await fetchData('/user/getUserToken', 'GET', null, token);
+        setToken(token);
+        setUser(resUser.data.user);
+        setTexts(resUser.data.texts);
+
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+
   return (
-    <div>Login</div>
+     <Container className='contenedor-login'>
+      <div className='div-log'>
+        <form className='login'>
+          <h2>Iniciar sesión</h2>
+          <hr />
+          <div className='input-div'>
+            <label htmlFor="email">Correo</label>
+            <input 
+              id='email' 
+              type="text"
+              name='email'
+              value={login.email}
+              onChange={handleChange}
+              />
+          </div>
+          <div className='input-div'>
+            <label htmlFor="password">Contraseña</label>
+            <div className='div-pass'>
+              <input
+                id='password'
+                type='password'
+                name='password'
+                value={login.password}
+                className='w-100'
+                onChange={handleChange}
+                 />
+                 <i 
+                    onClick={() => setShowPass(!showPass)}  
+                    className={showPass ? "bi bi-eye" :  "bi bi-eye-slash"}></i>
+            </div>
+          </div>
+          
+          <div className='btn-div'>
+            <Boton
+              valor="Cancelar"
+              aspecto="btn-err-1 w-50"
+              onClick={()=> navigate('/')}
+            />
+            <Boton
+              aspecto="btn-3 w-100"
+              valor="Acceder"
+              onClick={onSubmit}
+            />
+          </div>
+          <div className='link-sesion'>
+            <p>¿Aún no estás registrado? <Link to={'/register'} className='register-link'>Regístrate aquí</Link></p>
+          </div>
+        
+        </form>
+      </div>
+    </Container>
   )
 }
+
+
+
 
 export default Login;
--- server/modules/user/user.controllers.js
+++ server/modules/user/user.controllers.js
@@ -1,6 +1,6 @@
 import { sendMailConfirm } from "../../services/emailServices.js";
-import { hashString } from "../../utils/hashUtils.js";
-import { generateTokenConfirm } from "../../utils/tokenUtils.js";
+import { compareString, hashString } from "../../utils/hashUtils.js";
+import { generateTokenConfirm, generateTokenLogin } from "../../utils/tokenUtils.js";
 import userDal from './user.dal.js'
 
 class UserController{
@@ -27,6 +27,52 @@ class UserController{
         }
     }
 
+
+
+
+    //login
+    login = async(req, res) => {
+        try {    
+            const { email, password } = req.body;
+
+            //comprobar si existe ese email
+            const result = await userDal.findEmail(email);
+
+            if(result === 0){
+                res.status(401).json({message: 'Email no registrado'})
+            }else{//comprobar password
+                 const match = await compareString(password, result[0].password )
+
+                 if(!match){//si no es correcto
+                    res.status(401).json({message: 'Contraseña incorrecta'});
+                 }else{//genera un token
+                    const token = generateTokenLogin(result[0].user_id);
+                    res.status(200).json({token})
+                 }
+            }
+
+        } catch (error) {
+                 console.log(error);
+            res.status(500).json({
+                                message:"error de server",
+                                dataError: error
+                            })
+        }
+    }
+
+
+    //3.- login traer datos del user
+    getUserToken = async(req, res) => {
+        try {
+            
+        } catch (error) {
+             console.log(error);
+            res.status(500).json({
+                                message:"error de server",
+                                dataError: error
+                            })
+        }
+    }
     
 }
 
